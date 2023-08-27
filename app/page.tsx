fcde9abbcbfbf8fdac13b1c065fa6ef91bb0d736
Rufat Aliyev <rufat845@gmail.com>
2023-08-27 23:23:50 +0400

    msg

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
-import NowPlaying from "@/components/Homepage/NowPlaying";
 import { Card, CardHeader } from "@/components/ui/card";
 import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
-      <section id="hero">
-        <Suspense fallback={<p>Loading</p>}>
-          <NowPlaying />
-        </Suspense>
-      </section>
+      <section id="hero">h1</section>
     </>
   );
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,15 +1,10 @@
 "use client";
-import { SafeUser } from "@/types/types";
 import Logo from "./Logo";
 import Navlinks from "./Navlinks";
 
-export interface NavbarProps {
-  currentUser?: SafeUser | null;
-}
-
-const Header: React.FC<NavbarProps> = () => {
+const Header: React.FC = () => {
   return (
-    <header className="fixed backdrop-blur-[5px] top-0 left-0 z-50 w-full border-b border-slate-400/50">
+    <header className="fixed backdrop-blur-[5px] bg-white top-0 left-0 z-50 w-full border-b border-slate-400/50">
       <nav className="container relative z-10 items-center flex  gap-5 justify-between py-5 ">
         <Logo />
         <Navlinks />
