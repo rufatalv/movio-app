4d1e0700bd9dd20a0723b2532b51c0880279057d
Rufat Aliyev <rufat845@gmail.com>
2023-09-13 12:49:57 +0400

    init

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -24,7 +24,8 @@ export default function TabsDemo() {
             <CardHeader>
               <CardTitle>Account</CardTitle>
               <CardDescription>
-                Make changes to your account here. Click save when you're done.
+                Make changes to your account here. Click save when you&apos;re
+                done.
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
@@ -47,7 +48,8 @@ export default function TabsDemo() {
             <CardHeader>
               <CardTitle>Password</CardTitle>
               <CardDescription>
-                Change your password here. After saving, you'll be logged out.
+                Change your password here. After saving, you&apos;ll be logged
+                out.
               </CardDescription>
             </CardHeader>
             <CardContent className="space-y-2">
